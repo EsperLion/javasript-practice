c07dbc55b4ca784de80917be3b4652666979c2c9
EsperLion <germankushchlion31@gmail.com>
2017-12-15 20:24:08 +0200

    make a gameasdfasdfasdfasd

--- 08-ws-game/player.js
+++ 08-ws-game/player.js
@@ -5,6 +5,13 @@ var keyCodes = {
     arrowLeft: 37
 };
 
+var keyPressed = {
+    arrowUp: false,
+    arrowDown: false,
+    arrowRight: false,
+    arrowLeft: false
+};
+
 var fps = 60;
 var speed = 32;
 
@@ -13,23 +20,59 @@ var posy = 0;
 
 document.addEventListener("keydown", function (e) {
 
-    var player = document.querySelector(".player.player--you");
-    console.log(player.style.transform);
+    console.log('down', JSON.stringify(keyPressed));
+
     if (keyCodes.arrowUp === e.keyCode) {
-        console.log('Move up');
-        player.style.transform = "translate(" + (--posy) + "px)";
-    } else if (keyCodes.arrowDown === e.keyCode) {
-        console.log('Move down');
-        player.style.transform = "translate(" + (++posy) + "px)";
+        keyPressed.arrowUp = true;
+    }
+    if (keyCodes.arrowDown === e.keyCode) {
+        keyPressed[e.keyCode] = true;
     }
     if (keyCodes.arrowRight === e.keyCode) {
-        console.log('Move right');
-        player.style.transform = "translate(" + (++posx) + "px)";
-    } else if (keyCodes.arrowLeft === e.keyCode) {
-        console.log('Move left');
-        player.style.transform = "translate(" + (--posx) + "px)";
+        keyPressed[e.keyCode] = true;
+    }
+    if (keyCodes.arrowLeft === e.keyCode) {
+        keyPressed[e.keyCode] = true;
     }
 
 });
+
+document.addEventListener("keyup", function (e) {
+
+    console.log('up', JSON.stringify(keyPressed));
+
+    if (keyCodes.arrowUp === e.keyCode) {
+        keyPressed[e.keyCode] = false;
+    }
+    if (keyCodes.arrowDown === e.keyCode) {
+        keyPressed[e.keyCode] = false;
+    }
+    if (keyCodes.arrowRight === e.keyCode) {
+        keyPressed[e.keyCode] = false;
+    }
+    if (keyCodes.arrowLeft === e.keyCode) {
+        keyPressed[e.keyCode] = false;
+    }
+
+});
+
+setInterval(function () {
+
+    var player = document.querySelector(".player.player--you");
+
+    if (keyPressed.arrowUp) {
+        player.style.transform = "translate(" + (posx) + "px, " + (--posy) + "px)";
+    }
+    if (keyPressed.arrowDown) {
+        player.style.transform = "translate(" + (posx) + "px, " + (++posy) + "px)";
+    }
+    if (keyPressed.arrowRight) {
+        player.style.transform = "translate(" + (++posx) + "px, " + (posy) + "px)";
+    }
+    if (keyPressed.arrowLeft) {
+        player.style.transform = "translate(" + (--posx) + "px, " + (posy) + "px)";
+    }
+}, 20);
+
 
 

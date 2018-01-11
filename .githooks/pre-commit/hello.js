f818203a5a932f8f0c2d74956af14f05721e5c02
EsperLion <germankushchlion31@gmail.com>
2018-01-12 00:40:55 +0200

    asda

--- .githooks/pre-commit/hello.js
+++ .githooks/pre-commit/hello.js
@@ -1,2 +1,7 @@
-#!/usr/bin/env node 
+#!/usr/bin/env node
+console.log('-----');
 console.log(process.argv);
+console.log('-----');
+console.log('-----');
+console.log(process.env);
+console.log('-----');

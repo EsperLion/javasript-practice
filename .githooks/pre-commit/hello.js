b08d103add629fe5b4ab07ec4188c7d063c06a2f
EsperLion <germankushchlion31@gmail.com>
2018-01-12 00:49:17 +0200

    asda

--- .githooks/pre-commit/hello.js
+++ .githooks/pre-commit/hello.js
@@ -1,19 +1,6 @@
 #!/usr/bin/env node
 // console.log('-----');
-// console.log(process.argv);
-// console.log('-----');
-// console.log('-----');
 // console.log(process.env);
 // console.log('-----');
-// console.log('-----');
-// console.log(process.execArgv);
-// console.log('-----');
 
-process.stdin.setEncoding('utf8');
 
-process.stdin.on('readable', function ()  {
-	var chunk = process.stdin.read();
-	if (chunk !== null) {
-		process.stdout.write('data:' + chunk);
-	}
-});

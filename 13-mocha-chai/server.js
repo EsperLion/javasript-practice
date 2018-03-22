83912e55efd31b8d3381c4cf47873ecef02822a0
German Kushch <gkushch@thinktime.com>
2018-03-22 22:46:26 +0200

    aasdgas da13

--- 13-mocha-chai/server.js
+++ 13-mocha-chai/server.js
@@ -52,3 +52,113 @@ function createServer() {
 
 module.exports.create = createServer;
 module.exports.app = app;
+
+var express = require("express"),
+	bodyParser = require("body-parser");
+
+var each = require('lodash-es/ma')
+
+
+var app = express();
+
+let users = [
+	{
+		name: 'German',
+		age: 22
+	},
+	{
+		name: 'Greg',
+		age: 42
+	},
+	{
+		name: 'Vasya',
+		age: 26
+	}
+];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extedn: true}));
+
+app.get("/get-info", function (req, res) {
+	res
+		.status(200)
+		.json(users);
+});
+
+app.post("/add-user", function (req, res) {
+	users.push({
+		name: 'Nastya',
+		age: 22
+	});
+	res
+		.status(200)
+		.json(users);
+});
+
+function createServer() {
+	return new Promise((resolve, reject) => {
+		"use strict";
+		app.listen(8089, function () {
+			console.log("server started");
+			resolve();
+		});
+	});
+}
+
+module.exports.create = createServer;
+module.exports.app = app;
+
+var express = require("express"),
+	bodyParser = require("body-parser");
+
+var each = require('lodash-es/ma')
+
+
+var app = express();
+
+let users = [
+	{
+		name: 'German',
+		age: 22
+	},
+	{
+		name: 'Greg',
+		age: 42
+	},
+	{
+		name: 'Vasya',
+		age: 26
+	}
+];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extedn: true}));
+
+app.get("/get-info", function (req, res) {
+	res
+		.status(200)
+		.json(users);
+});
+
+app.post("/add-user", function (req, res) {
+	users.push({
+		name: 'Nastya',
+		age: 22
+	});
+	res
+		.status(200)
+		.json(users);
+});
+
+function createServer() {
+	return new Promise((resolve, reject) => {
+		"use strict";
+		app.listen(8089, function () {
+			console.log("server started");
+			resolve();
+		});
+	});
+}
+
+module.exports.create = createServer;
+module.exports.app = app;
